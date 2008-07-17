b872fdc0794e35873244ad6b87e4bc080bd5b1b3
Payal Gupta <cool.payal1985@gmail.com>
2008-07-17 18:16:52 +0530

    Select Box/Check Availability functionality

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,6 +4,7 @@ class LoginController < ApplicationController
   end
 
   def new
+    @sex = ["Male", "Female"]
     @user = User.new
   end
 
@@ -15,6 +16,23 @@ class LoginController < ApplicationController
     else
       render(:action => :new)
     end
+  end
+
+	def check_username_availability
+		@message = nil
+		@username = params[:user][:username]
+		if @username.blank?
+			@message = "Username should not be blank."
+		else
+		  if User.find_by_username(@username)
+		      @message = "Username not available"
+		  else
+		      @message = "Username available."
+		  end
+		end 
+   		render :update do |page|
+			page.replace_html 'testdiv', :partial => 'check_availability'
+		end
   end
 
   def login
