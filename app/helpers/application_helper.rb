08ae258542d6b8026c30b0db25ef8b8d26296854
Payal Gupta <cool.payal1985@gmail.com>
2008-07-21 16:48:27 +0530

    Modified notification logic and removed redundant partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,3 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
- def notifications
-  if flash[:success]
-    render :partial => 'layouts/success', :object => flash[:success]
-   elsif flash[:notice]
-    render :partial => 'layouts/notice', :object => flash[:notice]
-   elsif flash[:error]
-    render :partial => 'layouts/error', :object => flash[:error]
-   end
- end
 end
