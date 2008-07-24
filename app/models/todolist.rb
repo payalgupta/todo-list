ce7ddc2e1b9b2d5d77741d174ba97310ffaf0b33
Payal Gupta <cool.payal1985@gmail.com>
2008-07-24 10:09:09 +0530

    Todolist Description made optional

--- app/models/todolist.rb
+++ app/models/todolist.rb
@@ -1,6 +1,6 @@
 class Todolist < ActiveRecord::Base
 	belongs_to :user
 	has_many :listitems
-	validates_presence_of	:title, :description
+	validates_presence_of	:title
 	validates_uniqueness_of	:title, :scope => "user_id"
 end
